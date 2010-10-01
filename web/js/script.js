e80d1a09ade920a79fb525f45f2fe90cd02f95f9
Piers Warmers <hello@pierswarmers.com>
2010-10-01 15:50:54 +1000

    Updated handling for forms.

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,13 @@
 /* Author: 
 
 */
-
+$(function(){
+  /*
+    Section: Spice up the form rows to be highlighted.
+  */
+  $('.rt-form-row textarea,.rt-form-row input').focus(function(){
+    $(this).parents('li').addClass('rt-highlight');
+  }).blur(function(){
+    $(this).parents('li').removeClass('rt-highlight');
+  });
+});
