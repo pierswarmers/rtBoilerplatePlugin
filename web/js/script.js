ca5d8ec53c7fb2d1d9837e3fe3283f2753c91072
Piers Warmers <hello@pierswarmers.com>
2011-08-12 13:50:05 +1000

    Added large update of order process scripts.

--- web/js/script.js
+++ web/js/script.js
@@ -7,48 +7,94 @@
  *
  */
 
-$(function(){
-  /*
-    Section: Your code goes here...
-  */
+// Hide unstyled order form
+$("form.rt-shop-product-order-panel").hide();
 
-    //
-    $(window).resize(function() {
+/* Optiptimise screen layout
+================================================== */
+var rtOptiptimiseLayout = function() {
+    if ($(window).width() <= 520 && !$("#rtProductHolder").hasClass('microOptimised')) { $('.rt-shop-product-gallery').prependTo($("#rtProductHolder")); $("#rtProductHolder").addClass('microOptimised').removeClass('macroOptimised'); }
+    if ($(window).width() >  520 && !$("#rtProductHolder").hasClass('macroOptimised')) { $('.rt-shop-product-gallery').appendTo($("#rtProductHolder"));  $("#rtProductHolder").addClass('macroOptimised').removeClass('microOptimised'); }
+}
 
-        var holder = $("#rtProductHolder");
+/* Optiptimise screen layout
+================================================== */
+var rtAttributesValid = function() {
+  var i = 0; var button = $('form.rt-shop-product-order-panel button');
+  $(".rt-shop-option-set").each(function(){ $(this).children('input:checked').each(function(){ i++; }); });
+  if(i == $("form.rt-shop-product-order-panel .rt-shop-option-set").size()) {
+    if(rtAttributesAllSelectedSelected()) {
+      button.text("Add to Cart").attr("disabled",false).removeClass("disabled").next('span').hide();
+    } else {
+      button.text("Not available").attr("disabled",true).addClass("disabled").next('span').show().addClass('error').html('That combination isn\'t in stock!');
+    }
+  }
+}
 
-        if($(this).width() <= 520 && !holder.hasClass('microOptimised')) {
-            $('.rt-shop-product-gallery').prependTo($("#rtProductHolder"));
-            holder.addClass('microOptimised').removeClass('macroOptimised');
-        }
-        if($(this).width() > 520 && !holder.hasClass('macroOptimised')) {
-            $('.rt-shop-product-gallery').appendTo($("#rtProductHolder"));
-            holder.addClass('macroOptimised').removeClass('microOptimised');
-        }
+/* Are all options selected?
+================================================== */
+var rtAttributesAllSelectedSelected = function() {
+    var check = true;
+    $("form .rt-shop-option-set input:checked").each(function() { if(!$(this).button('widget').hasClass('available')) { check = false; } });
+    return check;
+}
+
+
+/* On load...
+================================================== */
+
+$(function() {
+
+    // rtOptiptimiseLayout after window resizes
+    $(window).resize(function() { rtOptiptimiseLayout(); });
+    
+    // rtOptiptimiseLayout on load
+    rtOptiptimiseLayout();
+
+    rtAttributesValid();
+
+    // Activate attribute selection buttons
+    $(".rt-shop-option-set").buttonset().find(':radio').click(function(e) {
+        $(".rt-shop-product-primary-image a[class*=rt-image-ref-" + $(this).attr("title").toLowerCase().replace(/[^a-zA-Z0-9]/g, "") + "]").css("display","inline").siblings('a').css("display","none");
+        $(".rt-shop-option-set input[type=radio]").each(function() { $(this).button( "widget" ).fadeTo(1, 0.4).removeClass('available'); });
+        $($(this).next('.ref').html()).each(function() { if(!$(this).hasClass('unavailable')) { $(this).button( "widget" ).fadeTo(1, 1).addClass('available'); } });
+        rtAttributesValid();
+    }).each(function() {
+        if($(this).button( "widget" ).hasClass('unavailable')) { $(this).button('disable', true).button( "widget" ).fadeTo(1, 0.4); }
     });
 
-    /*
-    * Skeleton V1.0.3
-    * Copyright 2011, Dave Gamache
-    * www.getskeleton.com
-    * Free to use under the MIT license.
-    * http://www.opensource.org/licenses/mit-license.php
-    * 7/17/2011
-    */
+    // Re-enable the order form... see earlier in this script
+    $("form.rt-shop-product-order-panel").show();
 
-	/* Tabs Activiation
-	================================================== */
-	var tabs = $('ul.tabs');
-	tabs.each(function(i) {
-		var tab = $(this).find('> li > a');
-		tab.click(function(e) {
-			var contentLocation = $(this).attr('href') + "Tab";
-			if(contentLocation.charAt(0)=="#") {
-				e.preventDefault();
-				tab.removeClass('active');
-				$(this).addClass('active');
-				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
-			}
-		});
-	});
+    // Handle clicks on the wishlist and the Ajax call to save the item.
+    $(".rt-shop-add-to-wishlist a").click(function(e) {
+        e.preventDefault(); $('.rt-shop-add-to-wishlist').addClass('loading').html('Adding to wishlist...');
+        $.ajax({ type: "POST", url: '/add-to-wishlist', data: ({ id : $('#rt-shop-product-id').attr('value') }), dataType: "xhr", success: function(data) { $('.rt-shop-add-to-wishlist').removeClass('loading').addClass('success'); $('.rt-shop-add-to-wishlist').html(data); } });
+      });
+
+    /*
+     * Skeleton V1.0.3
+     * Copyright 2011, Dave Gamache
+     * www.getskeleton.com
+     * Free to use under the MIT license.
+     * http://www.opensource.org/licenses/mit-license.php
+     * 7/17/2011
+     */
+
+    /* Tabs activiation
+     ================================================== */
+    var tabs = $('ul.tabs');
+    tabs.each(function(i) {
+        var tab = $(this).find('> li > a');
+        tab.click(function(e) {
+            var contentLocation = $(this).attr('href') + "Tab";
+            if (contentLocation.charAt(0) == "#") {
+                e.preventDefault();
+                tab.removeClass('active');
+                $(this).addClass('active');
+                $(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
+            }
+        });
+    });
 });
+
